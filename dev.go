1e824529ebd09d40a952e133898cfa8feaf22c89
892947707 <dswangadmin@163.com>
2021-09-16 21:17:32 +0800

    update

---
+++ dev.go
@@ -0,0 +1,12 @@
+package main
+ 
+import (
+	//青龙2.9.x机器人
+	_ "github.com/cdle/sillyGirl/develop/qinglong"
+	//
+	_ "github.com/cdle/sillyGirl/develop/jd_cookie"
+	//QQ
+	_ "github.com/cdle/sillyGirl/im/qq"
+	//tg
+	_ "github.com/cdle/sillyGirl/im/tg"
+	)
